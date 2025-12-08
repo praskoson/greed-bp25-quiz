37fe6a778e6fd254e4b1de2eede013474e37b44c
praskoson <prasinar@gmail.com>
2025-12-08 16:40:21 +0100

    logs for wallet connect errors

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -10,7 +10,11 @@ import { env } from "@/env";
 import { WalletAuthProvider } from "@/state/use-wallet-auth";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { getQueryClient } from "./get-query-client";
-import { WalletSignTransactionError } from "@solana/wallet-adapter-base";
+import {
+  WalletSignTransactionError,
+  WalletConnectionError,
+  WalletDisconnectionError,
+} from "@solana/wallet-adapter-base";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const DynamicModalProvider = dynamic(() =>
@@ -28,9 +32,13 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <WalletProvider
         wallets={[]}
         autoConnect
-        onError={(error) => {
-          if (error instanceof WalletSignTransactionError) {
-            console.log(error.name, error.message);
+        onError={(error, adapter) => {
+          if (
+            error instanceof WalletConnectionError ||
+            error instanceof WalletDisconnectionError ||
+            error instanceof WalletSignTransactionError
+          ) {
+            console.log(error.name, error.message, adapter);
           }
         }}
       >
