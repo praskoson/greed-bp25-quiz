63e29482204bad4d3d298d8dfe8a25170c677f43
praskoson <prasinar@gmail.com>
2025-12-08 11:51:50 +0100

    update script to use args

--- src/scripts/add_admin_user.ts
+++ src/scripts/add_admin_user.ts
@@ -2,20 +2,24 @@ import { loadEnvConfig } from "@next/env";
 
 loadEnvConfig(process.cwd());
 
-async function addAdminUser() {
+async function addAdminUser(email: string, name: string, password: string) {
   const { auth } = await import("../lib/admin-auth.js");
   const result = await auth.api.signUpEmail({
     body: {
-      email: "prasko@blastctrl.com",
-      name: "demo-admin",
-      password: "demo-admin",
+      email,
+      name,
+      password,
     },
   });
 
   console.log(result.user);
 }
 
-addAdminUser()
+const email = process.argv[2];
+const name = process.argv[3];
+const password = process.argv[4];
+
+addAdminUser(email, name, password)
   .then(() => {
     console.log("Done!");
     process.exit(0);
